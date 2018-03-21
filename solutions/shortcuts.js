c81f01e1445585cbd6fffc7df9f6b12a8992cb6f
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-03-21 17:21:58 -0400

    exercise 6 - shortcuts

---
+++ solutions/shortcuts.js
@@ -0,0 +1,29 @@
+// promise.then(null, function (err) {
+//   console.error('THERE IS AN ERROR!!!');
+//   console.error(err.message);
+// });
+
+// // You could simply write
+
+// promise.catch(function (err) {
+//   console.error('THERE IS AN ERROR!!!');
+//   console.error(err.message);
+// });
+
+
+// var promise = new Promise(function (fulfill, reject) {
+//   fulfill('SECRET VALUE');
+// });
+
+// // Introducing: Promise.resolve
+// // It does the exact same thing as above:
+
+// var promise = Promise.resolve('SECRET VALUE');
+
+// // Likewise...
+
+// var promise = new Promise(function (fulfill, reject) {
+//   reject(new Error('SECRET VALUE'));
+// });
+
+// var promise = Promise.reject(new Error('SECRET VALUE'));
