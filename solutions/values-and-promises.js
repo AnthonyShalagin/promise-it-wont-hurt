737d7299f827b949315821312a8a30f785f6cf84
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-03-26 10:21:17 -0400

    values and promises -exercise 8

---
+++ solutions/values-and-promises.js
@@ -0,0 +1,15 @@
+/*
+  * Declare a function `attachTitle` which prepends `'DR. '` to its firstargument and returns the result.
+  * Create a fulfilled promise with a value of `'MANHATTAN'`.
+  * Build a promise chain off the promise we just constructed that first calls`attachTitle` then calls `console.log`.
+*/
+
+'use strict';
+
+function attachTitle (arg) {
+	return 'DR. ' + arg;
+}
+
+Promise.resolve('MANHATTAN')
+	.then(attachTitle)
+	.then(console.log);
